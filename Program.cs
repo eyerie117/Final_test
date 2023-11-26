8f73638a53eafe7644c7643a3db8dc8203fd519a
Kseniia Esepenko <eyerie86@gmail.com>
2023-11-26 17:55:53 +0300

    add program code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿void ReleaseArray(string[] array)
+{
+    for (int i = 0; i < array.Length;)
+    {
+        if (array[i].Length > 3)
+        {
+            for (int j = i; j < array.Length - 1; j++)
+            {
+                array[j] = array[j + 1];
+            }
+            Array.Resize(ref array, array.Length - 1);
+        }
+        else if (array[i].Length <= 3)
+            i++;
+    }
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+Console.Clear();
+Console.WriteLine("Введите элементы массива через пробел: ");
+string[] arr = Console.ReadLine().Split(new char[] { ' ' });
+ReleaseArray(arr);
